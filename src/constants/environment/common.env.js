0299a72778e591bdc3b125aed6c5b7fd27cdb2e0
akams <e.ndjawe@gmail.com>
2020-12-01 18:35:43 +0100

    chore: check env to set apiUrl

--- src/constants/environment/common.env.js
+++ src/constants/environment/common.env.js
@@ -1,7 +1,13 @@
+const env = process.env.REACT_APP_ENVIRONMENT;
+
+const prodApi = 'https://us-central1-kombe-music.cloudfunctions.net';
+const localApi = 'http://localhost:5001/kombe-music/us-central1';
+
 const environment = {
   env: process.env,
-  apiUrl: 'https://us-central1-kombe-music.cloudfunctions.net/kombeMusicCF/api/v1',
-  localApiUrl: 'http://localhost:5001/kombe-music/us-central1/kombeMusicCF/api/v1',
+  apiUrl: `${env === 'DEV' ? localApi : prodApi}/kombeMusicCF/api/v1`,
 };
+
+console.log('apiUrl===>', environment.apiUrl);
 
 export default environment;
