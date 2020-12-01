157f3321da923198c2b0fc10e4e46c12ce1189ec
akams <e.ndjawe@gmail.com>
2020-12-01 14:35:06 +0100

    chore: add env local

--- src/constants/environment/common.env.js
+++ src/constants/environment/common.env.js
@@ -1,6 +1,7 @@
 const environment = {
   env: process.env,
   apiUrl: 'https://us-central1-kombe-music.cloudfunctions.net/kombeMusicCF/api/v1',
+  localApiUrl: 'http://localhost:5001/kombe-music/us-central1/kombeMusicCF/api/v1',
 };
 
 export default environment;
