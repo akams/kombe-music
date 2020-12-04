bb1868f20ff7adcad5bd54a5fc932fa9b0457337
akams <e.ndjawe@gmail.com>
2020-12-04 13:45:19 +0100

    feat: remove useless component

--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,5 @@
-import React, { useState } from 'react';
-import { Container, Form, FormGroup, Label, Input, InputGroup, Row, Col } from 'reactstrap';
+import React from 'react';
+import { Container, Row, Col } from 'reactstrap';
 
 import CardAlbum from '../../components/CardAlbum';
 
@@ -7,32 +7,11 @@ import './index.scss';
 
 function Home(props) {
   const { history, albums } = props;
-  const [title, setTitle] = useState('');
-
-  const onChange = (event) => {
-    const { value } = event.target;
-    setTitle(value);
-    // findArtist(value)
-    //   .then((res) => {
-    //     console.warn({ res });
-    //   })
-    //   .catch((e) => console.error(e));
-  };
 
   return (
     <div id="Home">
       <h1 className="text-white">Home Page</h1>
       <Container style={{ padding: '5%', textAlign: 'left' }} fluid>
-        <Form>
-          <FormGroup>
-            <InputGroup>
-              <Label for="title" hidden>
-                Titre
-              </Label>
-              <Input type="text" name="title" id="title" placeholder="Rechercher" value={title} onChange={onChange} />
-            </InputGroup>
-          </FormGroup>
-        </Form>
         <Row>
           <Col xs={12} className="pt-6 pb-4">
             <h1 className="text-white">Les derniers ajouts</h1>
