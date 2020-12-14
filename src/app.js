a839142d3ad7f13a0f33dcdf7336f328d0d2d1bb
akams <e.ndjawe@gmail.com>
2020-12-15 00:39:30 +0100

    minimum to deploy 2

--- src/app.js
+++ src/app.js
@@ -1,40 +1,11 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import { compose } from 'recompose';
-import { connect } from 'react-redux';
 
-import { NotFound } from './pages';
-import * as ROUTES from './constants/routes';
-import { IsUserRedirect, UnProtectedRoute } from './helpers/routes';
-import { withFirebase } from './context/firebase';
-import { useAuthListener } from './hooks';
-import { dispatchSetUsers } from './redux/action/user';
-
-function App(props) {
-  const { dispatch, firebase, dispatchSetUsersFunction } = props;
-  const { user } = useAuthListener(firebase, dispatchSetUsersFunction);
-  console.log({ user });
-  return user !== false ? (
-    <Router>
-      <Switch>
-        <UnProtectedRoute path={ROUTES.SIGN_UP_END}>
-          <div>
-            <h1>Hello</h1>
-          </div>
-        </UnProtectedRoute>
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </Router>
-  ) : (
-    <div className="loader">
-      <div className="preloader-preview-area" />
+function App() {
+  return (
+    <div>
+      <h1>Hello page 1</h1>
     </div>
   );
 }
 
-const mapDispatchToProps = {
-  dispatchSetUsersFunction: (user) => dispatchSetUsers(user),
-};
-const mapStateToProps = () => ({});
-
-export default compose(withFirebase, connect(mapStateToProps, mapDispatchToProps))(App);
+export default App;
